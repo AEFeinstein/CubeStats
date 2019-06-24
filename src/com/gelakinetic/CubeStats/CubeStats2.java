e15ad9edaa4ef83a91885670c9fadc83cbc23bd5
aefeinstein <gelakinetic@gmail.com>
2019-06-24 11:56:39 -0400

    [WIP] Added new calculator

---
+++ src/com/gelakinetic/CubeStats/CubeStats2.java
@@ -0,0 +1,206 @@
+/**
+ * Copyright 2016 Adam Feinstein
+ *
+ * This file is part of CubeStats.
+ *
+ * CubeStats is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CubeStats is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CubeStats.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.gelakinetic.CubeStats;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class CubeStats2 {
+
+    
+    @SuppressWarnings("unused")
+    private static class cardCounts {
+        double Sorcery;
+        double Enchantment;
+        double Land;
+        HashMap<Integer, Double> Creature = new HashMap<>();
+        double Instant;
+        double Planeswalker;
+        double Artifact;
+    }
+
+    /**
+     * All the types. They are in distinct buckets (i.e. Artifact Lands don't count
+     * as Artifacts. The search is ordered, so first it searches for all lands, then
+     * creatures NOT lands, then artifacts NOT creatures NOT lands, etc
+     */
+    private static final String[] TYPES = {
+            "Creature",
+            "Artifact",
+            "Enchantment",
+            "Instant",
+            "Sorcery",
+            "Planeswalker",
+            "Land", };
+
+    /**
+     * Given the database of Magic cards used by MTG Familiar and a cube size,
+     * figure out how many of each card color, type, and converted mana cost (for
+     * creatures) are necessary
+     *
+     * @param args
+     *            Unused
+     */
+    public static void main(String[] args) {
+        try {
+            // Get all the cards from the database
+            ArrayList<MtgCard> allCards = doDatabaseQuery();
+
+            // Bucket the results into the hash map
+            HashMap <String, cardCounts> counts = bucketDatabaseResults(allCards);
+            
+            // TODO scale everything to a cube size
+            
+            // Print the results
+            printResults(counts);
+            
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * TODO
+     * 
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    private static ArrayList<MtgCard> doDatabaseQuery() throws ClassNotFoundException, SQLException {
+        /* Open up the database */
+        Connection dbConnection = null;
+
+        Class.forName("org.sqlite.JDBC");
+        dbConnection = DriverManager.getConnection("jdbc:sqlite:mtg.db");
+
+        Statement statement = dbConnection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+                "select cards.color_identity, cards.rarity, cards.cmc, cards.supertype, cards.color, cards.manacost\r\n" +
+                "from cards join sets on cards.expansion = sets.code\r\n" + 
+                "where (\r\n" + 
+                "    sets.suggest_text_1 like '%Masters%' AND\r\n" + 
+                "    sets.online_only = 0 AND\r\n" + 
+                "    cards.number not like \"%B\")\r\n" + 
+                "order by cards.color_identity desc, cards.supertype desc, cards.cmc desc");
+
+        /* Objectify the results */
+        ArrayList<MtgCard> allCards = new ArrayList<>();
+        while (resultSet.next()) {
+            allCards.add(new MtgCard(resultSet));
+        }
+
+        /* Clean up */
+        resultSet.close();
+        statement.close();
+        
+        return allCards;
+    }
+    
+    /**
+     * TODO
+     * 
+     * @param allCards
+     * @return
+     */
+    private static HashMap<String, cardCounts> bucketDatabaseResults(ArrayList<MtgCard> allCards) {
+
+        HashMap<String, cardCounts> counts = new HashMap<>();
+        
+        // For each type
+        for (String targetType : TYPES) {
+            // Iterate over all the cards
+            for (int cIdx = 0; cIdx < allCards.size(); cIdx++) {
+                MtgCard cardToCheck = allCards.get(cIdx);
+                // If the type matches
+                if(cardToCheck.getType().toLowerCase().contains(targetType.toLowerCase())) {
+                    // Figure out what bucket to put this card in
+                    
+                    // Make sure this color bucket exists first
+                    if(!counts.containsKey(cardToCheck.getColorKey())) {
+                        counts.put(cardToCheck.getColorKey(), new cardCounts());
+                    }
+                    cardCounts colorBucket = counts.get(cardToCheck.getColorKey());
+                    
+                    switch (targetType) {
+                        case "Creature": {
+                            if(!colorBucket.Creature.containsKey(cardToCheck.getCmc())) {
+                                colorBucket.Creature.put(cardToCheck.getCmc(), (double) 0);
+                            }
+                            double currentCount = colorBucket.Creature.get(cardToCheck.getCmc());
+                            colorBucket.Creature.put(cardToCheck.getCmc(), currentCount + cardToCheck.getRarityMultiplier());
+                            break;
+                        }
+                        case "Artifact": {
+                            colorBucket.Artifact += cardToCheck.getRarityMultiplier();
+                            break;
+                        }
+                        case "Enchantment": {
+                            colorBucket.Enchantment += cardToCheck.getRarityMultiplier();
+                            break;
+                        }
+                        case "Instant": {
+                            colorBucket.Instant += cardToCheck.getRarityMultiplier();
+                            break;
+                        }
+                        case "Sorcery": {
+                            colorBucket.Sorcery += cardToCheck.getRarityMultiplier();
+                            break;
+                        }
+                        case "Planeswalker": {
+                            colorBucket.Planeswalker += cardToCheck.getRarityMultiplier();
+                            break;
+                        }
+                        case "Land": {
+                            colorBucket.Land += cardToCheck.getRarityMultiplier();
+                            break;
+                        }
+                    }
+                    
+                    // Remove the card and set the index back one
+                    allCards.remove(cIdx);
+                    cIdx--;
+                }
+            }
+        }
+        return counts;
+    }
+    
+    /**
+     * TODO
+     * 
+     * @param counts
+     */
+    private static void printResults(HashMap<String, cardCounts> counts) {
+        Gson gson = new GsonBuilder().enableComplexMapKeySerialization()
+                .setPrettyPrinting().create();
+        String str = gson.toJson(counts);
+        System.out.println(str);        
+    }
+}
---
+++ src/com/gelakinetic/CubeStats/MtgCard.java
@@ -0,0 +1,105 @@
+package com.gelakinetic.CubeStats;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+
+class MtgCard {
+
+    private String color;
+    private String colorIdentity;
+    private char rarity;
+    private int cmc;
+    private String supertype;
+
+    MtgCard(ResultSet cursor) throws SQLException {
+        this.color = cursor.getString(cursor.findColumn("color"));
+        this.colorIdentity = cursor.getString(cursor.findColumn("color_identity"));
+        this.rarity = (char) cursor.getInt(cursor.findColumn("rarity"));
+        this.cmc = cursor.getInt(cursor.findColumn("cmc"));
+        this.supertype = cursor.getString(cursor.findColumn("supertype"));
+        String manacost = cursor.getString(cursor.findColumn("manacost"));
+
+        if (null == manacost) {
+            System.out.println("null mana");
+        }
+
+        // Clean up color stuff (artifact, land, and colorless aren't colors)
+        this.color.replaceAll("[ALCalc]", "");
+
+        // Create a Pattern object to match manacost
+        Pattern r = Pattern.compile("[WUBRGwubrg]");
+
+        // Lands are always colorless
+        if (this.supertype.toLowerCase().contains("land")) {
+            this.color = "";
+            this.colorIdentity = "";
+        }
+        // Artifacts are colorless too
+        else if (this.supertype.toLowerCase().contains("artifact") && !r.matcher(manacost).matches()) {
+            this.color = "";
+            this.colorIdentity = "";
+        }
+    }
+
+    /**
+     * TODO
+     * 
+     * @return
+     */
+    double getRarityMultiplier() {
+        switch (rarity) {
+            case 'c':
+            case 'C':
+                return 10;
+            case 'u':
+            case 'U':
+                return 3;
+            case 'r':
+            case 'R':
+                return 7 / 8.0d;
+            case 'm':
+            case 'M':
+                return 1 / 8.0d;
+            default:
+                return 0;
+        }
+    }
+
+    int getCmc() {
+        return cmc;
+    }
+
+    String getType() {
+        return supertype;
+    }
+
+    /**
+     * TODO
+     * 
+     * @return
+     */
+    String getColorKey() {
+        if (null == color || color.isEmpty()) {
+            return "Colorless";
+        }
+        else if (color.length() > 1) {
+            return "Gold";
+        }
+        else {
+            switch (color) {
+                case "W":
+                    return "White";
+                case "U":
+                    return "Blue";
+                case "B":
+                    return "Black";
+                case "R":
+                    return "Red";
+                case "G":
+                    return "Green";
+            }
+        }
+        return "Colorless";
+    }
+}
